20db9899f16aef4ceb47b0c9ab10c9bf102186fb
Elia-Fissi <elia.fissi94@hotmail.it>
2023-09-14 09:50:14 +0200

    fine

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('.btnLogin-popup');
 const iconClose = document.querySelector('.icon-close');
-
+let cards = document.querySelectorAll(".card");
+let wrappercards = document.querySelector("#wrappercards");
+console.log (wrappercards)
 registerLink.addEventListener('click', ()=> {
     wrapper.classList.add('active');
 });
@@ -14,9 +16,20 @@ loginLink.addEventListener('click', ()=> {
 });
 
 btnPopup.addEventListener('click', ()=> {
+    wrapper.classList.remove("d-none")
     wrapper.classList.add('active-popup');
+    wrappercards.classList.add("d-none");
 });
 
 iconClose.addEventListener('click', ()=> {
     wrapper.classList.remove('active-popup');
+    setTimeout(() => {
+     wrapper.classList.add("d-none")   
+    }, 500);
+    setTimeout(() => {
+    wrappercards.classList.remove("d-none");
+        
+    }, 500);
+
 });
+
